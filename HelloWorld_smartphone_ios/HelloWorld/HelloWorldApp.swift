8cc04e9028ae860bcd79de2698673f8200adee1e
Jonas Lendzion Schultz II <jonasschultz1309@gmail.com>
2023-11-27 15:38:34 -0300

    add smartphone software and updated report

---
+++ HelloWorld_smartphone_ios/HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Jonas Lendzion Schultz II on 27/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
